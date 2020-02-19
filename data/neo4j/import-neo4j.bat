2e93606e18c33dfc70844532c651fb7c69f144da
unknown <shoubin@iscas.ac.cn>
2020-02-19 16:18:37 +0800

    add neo4j knowledge Graph

---
+++ data/neo4j/import-neo4j.bat
@@ -0,0 +1,3 @@
+C:\software\neo4j-community-3.5.6\bin\neo4j-import --into rfc.db --nodes rfc.csv --nodes author.csv --nodes header.csv --relationships rel_author.csv  --relationships rel_field.csv  --relationships rel_next.csv  --relationships rel_ref.csv  --relationships rel_similiar.csv
+
+@pause
